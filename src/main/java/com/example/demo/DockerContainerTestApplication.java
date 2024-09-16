3d7c81e323f31d75a0028121d93a02517b7ac7ea
marcusslomka <marcus.slomka@googlemail.com>
2024-09-16 13:55:20 +0200

    first push

---
+++ src/main/java/com/example/demo/DockerContainerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerContainerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerContainerTestApplication.class, args);
+	}
+
+}
